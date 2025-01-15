4ebbeff0e2e44750f64da9c26eb30b4be0e8fe8c
Branden Kaestner <12478583+brandenk514@users.noreply.github.com>
2025-01-15 10:07:07 -0800

    Added Tdarr

--- main.tf
+++ main.tf
@@ -8,19 +8,234 @@ terraform {
 }
 
 provider "docker" {
-  host = var.docker_host
+  host     = var.docker_host
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", var.ssh_key_path]
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
+resource "docker_network" "mgmt_net" {
+  name   = "mgmt_net"
+  driver = "bridge"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "nginx_test"
+resource "docker_network" "flix_net" {
+  name   = "flix_net"
+  driver = "bridge"
+}
+
+resource "docker_image" "portainer" {
+  name = "portainer/portainer-ce:2.21.5"
+}
+
+resource "docker_container" "portainer" {
+  image = docker_image.portainer.image_id
+  name  = "portainer"
   ports {
-    internal = 80
-    external = 81
+    internal = 8000
+    external = 8000
+  }
+  ports {
+    internal = 9000
+    external = 9000
+  }
+  ports {
+    internal = 9443
+    external = 9443
+  }
+  networks_advanced {
+    name    = docker_network.mgmt_net.name
+    aliases = ["portainer"]
+  }
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+  volumes {
+    volume_name    = "portainer_data"
+    container_path = "/data"
+  }
+}
+
+resource "docker_image" "uptime_kuma" {
+  name = "louislam/uptime-kuma:1.23.16"
+}
+
+resource "docker_container" "uptime_kuma" {
+  image   = docker_image.uptime_kuma.image_id
+  name    = "uptime-kuma"
+  restart = "unless-stopped"
+  ports {
+    internal = 3001
+    external = 3001
+  }
+  volumes {
+    volume_name    = "uptime_kuma_data"
+    container_path = "/app/data"
+  }
+}
+
+resource "docker_image" "semaphore_ui" {
+  name = "semaphoreui/semaphore:v2.11.2"
+}
+
+resource "docker_container" "semaphore_ui" {
+  image = docker_image.semaphore_ui.image_id
+  name  = "semaphore-ui"
+  restart = "unless-stopped"
+  ports {
+    internal = 3000
+    external = 30080
+  }
+  env = [
+    "SEMAPHORE_DB_DIALECT=bolt",
+    "SEMAPHORE_ADMIN=${var.semaphore_admin}",
+    "SEMAPHORE_ADMIN_PASSWORD=${var.semaphore_admin_password}",
+    "SEMAPHORE_ADMIN_NAME=${var.semaphore_admin_name}",
+    "SEMAPHORE_ADMIN_EMAIL=${var.semaphore_admin_email}"
+  ]
+  volumes {
+    volume_name    = "semaphore_data"
+    container_path = "/var/lib/semaphore"
+  }
+  volumes {
+    volume_name    = "semaphore_config"
+    container_path = "/etc/semaphore"
+  }
+  volumes {
+    volume_name    = "semaphore_tmp"
+    container_path = "/tmp/semaphore"
+  }
+}
+
+resource "docker_image" "tdarr" {
+  name = "ghcr.io/haveagitgat/tdarr:latest"
+}
+
+resource "docker_container" "tdarr" {
+  image        = docker_image.tdarr.image_id
+  name         = "tdarr"
+  restart      = "unless-stopped"
+  network_mode = "bridge"
+  networks_advanced {
+    name    = docker_network.flix_net.name
+    aliases = ["tdarr"]
+  }
+  ports {
+    internal = 8265
+    external = 8265
+  }
+  ports {
+    internal = 8266
+    external = 8266
+  }
+  env = [
+    "TZ=America/Los_Angeles",
+    "PUID=1000",
+    "PGID=1000",
+    "UMASK_SET=002",
+    "serverIP=0.0.0.0",
+    "serverPort=8266",
+    "webUIPort=8265",
+    "internalNode=false",
+    "inContainer=true",
+    "ffmpegVersion=6",
+    "nodeName=dstack-tdarr",
+  ]
+  volumes {
+    volume_name    = "tdarr_data"
+    container_path = "/app/server"
+  }
+  volumes {
+    volume_name    = "tdarr_config"
+    container_path = "/app/configs"
+  }
+  volumes {
+    volume_name    = "tdarr_logs"
+    container_path = "/app/logs"
+  }
+  volumes {
+    host_path      = "/media"
+    container_path = "/media"
+  }
+  volumes {
+    volume_name    = "transcode_cache"
+    container_path = "/temp"
+  }
+}
+
+resource "docker_image" "tdarr_node" {
+  name = "ghcr.io/haveagitgat/tdarr_node:latest"
+}
+
+resource "docker_container" "tdarr_node_mov" {
+  image   = docker_image.tdarr_node.image_id
+  name    = "tdarr-node_mov"
+  restart = "unless-stopped"
+  networks_advanced {
+    name    = docker_network.flix_net.name
+    aliases = ["tdarr-node_mov"]
+  }
+  env = [
+    "TZ=America/Los_Angeles",
+    "PUID=1000",
+    "PGID=1000",
+    "UMASK_SET=002",
+    "nodeName=dstack-tdarr-node_mov",
+    "serverIP=${docker_container.tdarr.network_data.0.ip_address}",
+    "serverPort=8266",
+    "inContainer=true",
+    "ffmpegVersion=6",
+  ]
+  volumes {
+    volume_name    = "tdarr_node_configs"
+    container_path = "/app/configs"
+  }
+  volumes {
+    volume_name    = "tdarr_node_logs"
+    container_path = "/app/logs"
+  }
+  volumes {
+    host_path      = "/media"
+    container_path = "/media"
+  }
+  volumes {
+    volume_name    = "node_transcode_cache"
+    container_path = "/temp"
+  }
+}
+
+resource "docker_container" "tdarr_node_tv" {
+  image   = docker_image.tdarr_node.image_id
+  name    = "tdarr-node_tv"
+  restart = "unless-stopped"
+  networks_advanced {
+    name    = docker_network.flix_net.name
+    aliases = ["tdarr-node_tv"]
+  }
+  env = [
+    "TZ=America/Los_Angeles",
+    "PUID=1000",
+    "PGID=1000",
+    "UMASK_SET=002",
+    "nodeName=dstack-tdarr-node_tv",
+    "serverIP=${docker_container.tdarr.network_data.0.ip_address}",
+    "serverPort=8266",
+    "inContainer=true",
+    "ffmpegVersion=6",
+  ]
+  volumes {
+    volume_name    = "tdarr_node_configs_tv"
+    container_path = "/app/configs"
+  }
+  volumes {
+    volume_name    = "tdarr_node_logs_tv"
+    container_path = "/app/logs"
+  }
+  volumes {
+    host_path      = "/media"
+    container_path = "/media"
+  }
+  volumes {
+    volume_name    = "node_transcode_cache_tv"
+    container_path = "/temp"
   }
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,29 @@ variable "ssh_key_path" {
   description = "The URL of the remote Docker host"
   type        = string
 }
+
+variable "semaphore_admin" {
+  description = "Semaphore admin username"
+  type        = string
+}
+
+variable "semaphore_admin_password" {
+  description = "Semaphore admin password"
+  type        = string
+  sensitive   = true
+}
+
+variable "semaphore_admin_name" {
+  description = "Semaphore admin name"
+  type        = string
+}
+
+variable "semaphore_admin_email" {
+  description = "Semaphore admin email"
+  type        = string
+}
+
+variable "media_server" {
+  description = "The URL of the remote Docker host"
+  type        = string
+}
